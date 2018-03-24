a7d2e2d8e5e297fd512682f4cde89b16401e3186
Matthew Deacon <mdeacon@mezzanineware.com>
2018-03-24 15:38:11 +0200

    wallet-copy

---
+++ app/routes/drawerNavigator.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {ScrollView, View, StyleSheet} from 'react-native'
+import {DrawerNavigator, DrawerItems} from 'react-navigation'
+import Home from './../screens/home/home'
+import Deposit from './../screens/deposit/deposit'
+import Settings from './../screens/settings/settings'
+import Withdraw from './../screens/withdraw/withdraw'
+import About from './../screens/about/about'
+import Accounts from './../screens/accounts/accounts'
+import NewAccounts from './../screens/accounts/newAccount'
+import Receive from './../screens/receive/receive'
+import Logout from './../screens/auth/logout'
+import DrawerHeader from './../components/drawerHeader'
+import GetVerified from './../screens/settings/getVerified/getVerified2'
+import Colors from './../config/colors'
+import Currencies from './../screens/accounts/accountsB'
+
+const RouteConfigs = {
+    Home: {
+        screen: Home,
+    },
+    Accounts: {
+        screen: NewAccounts,
+    },
+    Currencies:{
+        screen:Currencies
+    },
+    Deposit: {
+        screen: Deposit,
+    },
+    Withdraw: {
+        screen: Withdraw,
+    },
+    Receive: {
+        screen: Receive,
+    },
+    GetVerified:{
+        screen: GetVerified,
+    },
+    Settings: {
+        screen: Settings,
+    },
+    About: {
+        screen: About,
+    },
+    Logout: {
+        screen: Logout,
+    },
+}
+
+export default DrawerNavigator(RouteConfigs, {
+    drawerWidth: 300,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle',
+    contentComponent: (props) => (
+        <View style={styles.container}>
+            <DrawerHeader navigation={props.navigation}/>
+            <ScrollView>
+                <DrawerItems
+                    {...props}
+                    activeTintColor="#6EBDF7"
+                    activeBackgroundColor="#485159"
+                    inactiveTintColor="white"
+                    inactiveBackgroundColor="transparent"
+                    labelStyle={{margin: 15, alignItems: 'center', fontSize: 16, fontWeight: 'normal'}}
+                />
+            </ScrollView>
+        </View>
+    ),
+})
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.drawerColor,
+    },
+})
---
+++ app/services/transactionService.js
@@ -0,0 +1,37 @@
+import BaseService from './baseService'
+
+var transactionService = {
+
+    getAllTransactionsByCurrecny: (code) => {
+        return BaseService.get('transactions/?currency=' + code)
+    },
+
+    getAllTransactions: () => {
+        return BaseService.get('transactions/')
+    },
+
+    getNextTransactions: (url) => {
+        return BaseService.getWithFullUrl(url);
+    },
+
+    sendMoney: (amount, recipient, note, currency, debit_account) => {
+        var data = {
+            amount,
+            recipient,
+            note,
+            currency,
+            debit_account
+        }
+        return BaseService.post('transactions/transfer/', data)
+    },
+
+    withdraw: (amount, reference) => {
+        var data = {
+            amount,
+            reference,
+        }
+        return BaseService.post('transactions/debit/', data)
+    },
+}
+
+export default transactionService
